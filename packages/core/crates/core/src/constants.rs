f3dc55da6c36a3b59fc1b33be3adda6d513513a5
Lucas Vieira <lucasvtiradentes@gmail.com>
2025-11-20 02:13:23 -0300

    chor: update constants

--- packages/core/crates/core/src/constants.rs
+++ packages/core/crates/core/src/constants.rs
@@ -9,7 +9,8 @@ pub const CONFIG_FILE_NAME: &str = "rules.json";
 
 pub const EXTENSION_PUBLISHER: &str = "lucasvtiradentes";
 pub const EXTENSION_NAME: &str = "tscanner-vscode";
-pub const DEV_SUFFIX: &str = "-dev";
+pub const EXTENSION_NAME_DEV: &str = "tscanner-vscode-dev";
+pub const DEV_SUFFIX: &str = "Dev";
 
 pub const LOG_BASENAME: &str = "tscannerlogs";
 
@@ -19,7 +20,7 @@ pub fn is_dev_mode() -> bool {
 
 pub fn get_vscode_extension_id() -> String {
     if is_dev_mode() {
-        format!("{}.{}{}", EXTENSION_PUBLISHER, EXTENSION_NAME, DEV_SUFFIX)
+        format!("{}.{}", EXTENSION_PUBLISHER, EXTENSION_NAME_DEV)
     } else {
         format!("{}.{}", EXTENSION_PUBLISHER, EXTENSION_NAME)
     }
